b97c58c27ef72a08b8e32aaff4f3b3a2bc643945
MatheusMartinsDeveloper <matheus2005brr@gmail.com>
2024-11-18 04:57:52 -0300

    feat: Make connection with MongoDB

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client";
 
 const PORT = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.listen(PORT, () => {
-    console.log(`Server Running in PORT:`, PORT);
-});
+async function main() {
+    app.listen(PORT, () => {
+        console.log(`Server Running in PORT:`, PORT);
+    });
+}
+
+main()
+    .catch((error) => {
+        console.error(`Prisma Error:`, error);
+        process.exit(1);
+    })
