eaaf752dcf2d439fe9c4480538922c492c9ece2f
MatheusMartinsDeveloper <matheus2005brr@gmail.com>
2024-11-21 07:27:39 -0300

    feat: Add new models in database and add function for authorization operation of admin

--- src/routes/Admin/adminRoutes.js
+++ src/routes/Admin/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import adminController from "../../controllers/Admin/adminController.js";
+import authenticate from "../../middlewares/Admin/authenticate.js";
 
 const adminRoutes = express.Router();
 
-adminRoutes.post("/createAdmin", adminController.create);
+adminRoutes.post("/createAdmin", authenticate, adminController.create);
 adminRoutes.post("/loginAdmin", adminController.login);
 
 export default adminRoutes;
